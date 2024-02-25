d3cb13e63416ca06a2acaef947da4fd3d2e0746c
jacky7584 <s1999111044@gmail.com>
2024-02-25 15:31:36 +0800

    feat: add CRUD API and SwaggerUI update

--- src/main/java/com/ntust/application/controller/MemberController.java
+++ src/main/java/com/ntust/application/controller/MemberController.java
@@ -1,23 +1,64 @@
 package com.ntust.application.controller;
 
+import com.ntust.application.controller.dio.CreateMemberDio;
+import com.ntust.application.controller.dio.UpdateMemberDio;
+import com.ntust.application.controller.dto.GetMemberDto;
 import com.ntust.application.dao.entity.MemberDo;
-import com.ntust.application.worker.UserWorker;
+import com.ntust.application.service.member.CreateMemberService;
+import com.ntust.application.service.member.DeleteMemberService;
+import com.ntust.application.service.member.GetMemberService;
+import com.ntust.application.service.member.UpdateMemberService;
+import com.ntust.application.worker.MemberWorker;
+import com.ntust.base.service.dto.ResponseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/member")
+@Api(tags="Member")
+@RequestMapping("member")
 @RequiredArgsConstructor
 public class MemberController {
-    private final UserWorker userWorker;
-    @GetMapping("/{id}")
-    public MemberDo getMember(@PathVariable Long id) {
-        MemberDo memberDo = userWorker.getUserById(id);
+    private final MemberWorker memberWorker;
 
-        return memberDo;
+    private final GetMemberService getMemberService;
+
+    @ApiOperation(value="get member data")
+    @ApiImplicitParam(name="id", value="ID", required = true, paramType = "path", dataTypeClass = Long.class)
+    @GetMapping("/{id}")
+    public ResponseDto<GetMemberDto>  getMember(@PathVariable Long id) {
+
+        return getMemberService.request(id);
+    }
+
+    private final CreateMemberService createMemberService;
+
+    @ApiOperation(value="create member data")
+    @PostMapping("")
+    public ResponseDto<Long> createMember(CreateMemberDio input) {
+
+        return createMemberService.request(input);
+    }
+
+    private final DeleteMemberService deleteMemberService;
+
+    @ApiOperation(value="delete member data")
+    @ApiImplicitParam(name="id", value="ID", required = true, paramType = "path", dataTypeClass = Long.class)
+    @DeleteMapping("/{id}")
+    public ResponseDto<Void> deleteMember(@PathVariable Long id) {
+        return deleteMemberService.request(id);
+    }
+
+    private final UpdateMemberService updateMemberService;
+
+    @ApiOperation(value="update member data")
+    @ApiImplicitParam(name="id", value="ID", required = true, paramType = "path", dataTypeClass = Long.class)
+    @PatchMapping("/{id}")
+    public ResponseDto<Void> updateMember(@PathVariable Long id, @RequestBody UpdateMemberDio input) {
+        input.setId(id);
+
+        return updateMemberService.request(input);
     }
 }
---
+++ src/main/java/com/ntust/application/worker/MemberWorker.java
@@ -0,0 +1,49 @@
+package com.ntust.application.worker;
+
+import com.ntust.application.controller.dio.UpdateMemberDio;
+import com.ntust.application.dao.entity.MemberDo;
+import com.ntust.application.dao.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberWorker {
+    private final MemberRepository memberRepository;
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public MemberDo updateMember(UpdateMemberDio input) {
+        MemberDo member = memberRepository.findById(input.getId()).orElse(null);
+
+        member.setUserName(input.getName());
+        member.setUserEmail(input.getEmail());
+
+        member = memberRepository.save(member);
+
+        return member;
+    }
+
+    public MemberDo createMember(String name, String email) {
+        MemberDo member = new MemberDo();
+        member.setUserName(name);
+        member.setUserEmail(email);
+
+        member = memberRepository.save(member);
+
+        return member;
+    }
+
+    public MemberDo updateUser(MemberDo user) {
+        user = memberRepository.save(user);
+
+        return user;
+    }
+
+
+    public MemberDo getMemberById(Long id) {
+        return memberRepository.findById(id).orElse(null);
+    }
+}
