f3e561e0fbc6b620ec2301c43487564a80220eab
wwgjob <howard890612@gmail.com>
2024-12-03 20:44:04 +0800

    feat: singleton test

---
+++ src/test/java/com/ntust/application/worker/SingletonTest.java
@@ -0,0 +1,11 @@
+package com.ntust.application.worker;
+
+public class SingletonTest {
+    private final Singleton singleton;
+
+
+    public SingletonTest(Singleton singleton) {
+        this.singleton = singleton;
+    }
+}
+
